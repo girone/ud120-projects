0325b84849b0c4c40dfe49d01ea6c58425de406f
Jonas Sternisko <jonas.sternisko@gmail.com>
2018-03-07 09:32:49 +0100

    Lecture progress

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
@@ -17,28 +18,38 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+internal_clf = RandomForestClassifier(criterion="entropy", min_samples_leaf=0.1)
+clf = AdaBoostClassifier(base_estimator=internal_clf, 
+                         n_estimators=15,
+                         algorithm="SAMME.R")
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time:", time() - t0)
+
+t0 = time()
+labels = clf.predict(features_test)
+print("Prediction time:", time() - t0)
 
 
+from sklearn.metrics import accuracy_score
+print("Accuracy:", accuracy_score(labels_test, labels))
 
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+prettyPicture(clf, features_test, labels_test)
+# try:
+    
+# except NameError:
+#     pass
