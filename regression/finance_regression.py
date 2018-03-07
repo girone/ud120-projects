ba8d44c2a4a6e9b981a23099ddfabc174c24373e
Jonas Sternisko <jonas.sternisko@gmail.com>
2018-03-07 09:33:27 +0100

    Lecture progress (regression)

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 """
     Starter code for the regression mini-project.
     
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,9 +38,16 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
+print(reg.coef_)
+print(reg.intercept_)
 
-
+# score
+print("Score on training set:", reg.score(feature_train, target_train))
+print("Score on test set:", reg.score(feature_test, target_test))
 
 
 
@@ -64,7 +72,17 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+# Hack to demonstrate outlier influence
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print(reg.coef_)
+print(reg.intercept_)
+# end of hack
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
