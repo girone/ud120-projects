3260d5181eac58254782b2e8d21ec3cdf97e5972
Jonas Sternisko <jonas.sternisko@gmail.com>
2018-04-19 12:08:14 +0200

    Lecture progress (clustering)

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -4,9 +4,6 @@
     Skeleton code for k-means clustering mini-project.
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
@@ -44,12 +41,24 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 data_dict.pop("TOTAL", 0)
 
 
+
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
+
+def find_min_max_ignore_NaN(data, feature_name):
+    feature_data = numpy.array([v[feature_name] for v in data.values() if v[feature_name] != "NaN"])
+    return feature_data.min(), feature_data.max()
+
+print("Feature ranges for feature-scaling (next lesson):")
+print(feature_2, "min, max:", find_min_max_ignore_NaN(data_dict, feature_2))
+print(feature_1, "min, max:", find_min_max_ignore_NaN(data_dict, feature_1))
+print(feature_3, "min, max:", find_min_max_ignore_NaN(data_dict, feature_3))
+
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,12 +67,17 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans 
+kmeans = KMeans(n_clusters=2, n_init=10)
+kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
+
 
 
 
