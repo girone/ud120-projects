f61b97163f1a4329b52ddc59b0623af52eb8c8a2
Jonas Sternisko <jonas.sternisko@gmail.com>
2018-05-15 01:07:04 +0200

    Lecture progress (evaluation)

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,25 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### code from previous lecture
+from sklearn.model_selection import train_test_split
+data_train, data_test, labels_train, labels_test = train_test_split(features, labels, random_state=42, test_size=0.3)
+
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(data_train, labels_train)
+
+predicted_labels = clf.predict(data_test)
+
+print sum(predicted_labels), len(predicted_labels)
+
+for i, v in enumerate(predicted_labels):
+    if v == 1. and v == labels_test[i]:
+        print 1
 
 
 ### your code goes here 
-
-
+from sklearn.metrics import precision_score, recall_score
+print "precision_score:", precision_score(predicted_labels, labels_test)
+print "recall_score:", recall_score(predicted_labels, labels_test)
